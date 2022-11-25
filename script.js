b9fd112539a24715bf945de7681fa38a42b6a764
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2022-11-25 17:13:26 -0300

    Armazenamento das horas na cache

--- script.js
+++ script.js
@@ -24,6 +24,12 @@ const inputPassword = document.querySelector('#id-password');
 const inputName = document.querySelector('#id-name');
 const dataHora = document.querySelector('#data-hora');
 
+// DOM Modal
+const modal = document.querySelector('.modal');
+const modalText = document.querySelector('.modal h2');
+const modalSpan = document.querySelector('.modal span');
+const modalButton = document.querySelector('.modal button');
+
 /* Pega o target através da desestruturação de objeto, que veio do event*/
 const validateInput = ({target}) => {
     if (target.value.length > 2) {
@@ -39,6 +45,8 @@ const handleSubmit = (event) => {
     /* Evita o funcionamento default, que no caso seria o de recarregamento da tela */
     event.preventDefault();
 
+    var currentDate = new Date();
+
     /* O browser possui uma ferramenta chamada Local Storage, que armazena dados da sessão, através dessa ferramenta será possivel armazenar os dados informados pelo usuário*/
 
     /* Pega o que o usuario digitou e salva no Local Storage pelo método localStorage.setItem(), que precisa de dois parâmetros, o primeiro é a chave do valor que será salvo e o segundo é o proprio valor*/
@@ -52,57 +60,86 @@ const handleSubmit = (event) => {
         hours: ""
     }
 
-    /*
-    dados.name = inputName.value;
-    dados.password = inputPassword.value;
-    dados.date = dataHora.innerHTML;*/
-
     if (localStorage.hasOwnProperty("registroponto")) {
         dados = JSON.parse(localStorage.getItem("registroponto"));
+        
         if (dados.name == inputName.value) {
             if (dados.password == inputPassword.value) {
-                console.log("Achou")
+                
+                if (dados.type == "entrada") {
+                    var timeTrab = (Date.parse(currentDate)) - 
+                                   (Date.parse(new Date(dados.date)));
+
+                    //console.log(timeTrab);
+                    //console.log(millisToHoursMinutesAndSeconds(timeTrab));
+                    dados.hours = dados.hours + timeTrab;
+
+                    dados.date = currentDate;
+
+                    dados.type = "saida";
+
+                    modalText.innerHTML = "Saída";
+                } else {
+                    dados.date = currentDate;
+
+                    dados.type = "entrada";
+
+                    modalText.innerHTML = "Entrada";
+                }
+
             } else {
-                console.log("Senha para o úsuario incorreta!");
+                alert("Senha para o úsuario incorreta!");
             }
         }
     } else {
         dados.type = "entrada";
         dados.name = inputName.value;
         dados.password = inputPassword.value;
-    }
+        dados.date = currentDate;
+        dados.hours = 0;
 
-    /* Adiciona um novo valor no array criado */
-    //dados.push({date: dataHora.innerHTML})
-    dados.date = dataHora.innerHTML;
-
-    if (dados.type == "saida") {
-        
+        modalText.innerHTML = "Entrada";
     }
 
     /* Salva o item */
     localStorage.setItem("registroponto", JSON.stringify(dados))      
-    
-    /* Redireciona o usuario para a pagina principal do jogo */
-    //window.location = 'pages/game.html'
 
     // Mostra o modal
+    modalSpan.innerHTML = "Total de horas: " + convertMsToTime(dados.hours);
     modal.style.display = 'block';
 }
 
-console.log(new Date());
-//console.log(Date.parse(new Date()) - 1669309896000);
-console.log(new Date(1669309896000))
-console.log((Date.parse(new Date()) - 1669309896000)/60000);
 
-function millisToMinutesAndSeconds(millis) {
-    var minutes = Math.floor(millis / 60000);
-    var seconds = ((millis % 60000) / 1000).toFixed(0);
-    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-  }
+//console.log(changeTimeZone(new Date(), 'America/Sao_Paulo'))
+//console.log(Date.parse(new Date()));
+//console.log(new Date(""));
 
-/*Thu Nov 24 2022 14:11:36 GMT-0300 (Horário Padrão de Brasília)
-1669309896000*/
+/*console.log(new Date(1669309896000))
+console.log(millisToMinutesAndSeconds(Date.parse(new Date("Thu Nov 24 2022 15:30:38 GMT-0300")) - 1669309896000));*/
+
+// Função que coloca dois digitos
+function padTo2Digits(num) {
+    return num.toString().padStart(2, '0');
+}
+// Outra função que coloca dois digitos
+function twoDigitsTime(time) {
+    return (time < 10 ? '0' : '') + time;
+}
+  
+// Função que converte Milissegundos para Tempo
+function convertMsToTime(milliseconds) {
+    let seconds = Math.floor(milliseconds / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+  
+    seconds = seconds % 60;
+    minutes = minutes % 60;
+    hours = hours % 24;
+  
+    return `${padTo2Digits(hours)}:${padTo2Digits(minutes)}:${padTo2Digits(
+      seconds,
+    )}`;
+}
 
 /* Executa a função abaixo toda vez que algo for digitado no input através da função 'input'
 Esse eventListener chama a função declarada na constante validateInput */
@@ -113,10 +150,6 @@ form.addEventListener('submit', handleSubmit);
 
 
 // Modal
-
-const modal = document.querySelector('.modal');
-const modalText = document.querySelector('.modal h2');
-const modalButton = document.querySelector('.modal button');
 
 function clicouOk() {
     modal.style.display = 'none';
@@ -142,7 +175,7 @@ function atualizaRelogio(){
     if (vminuto < 10){ vminuto = "0" + vminuto;}
     if (vsegundo < 10){ vsegundo = "0" + vsegundo;}
 
-    dataFormat = vdia + " / " + vmes + " / " + vano;
+    dataFormat = vdia + "/" + vmes + "/" + vano;
     horaFormat = vhora + " : " + vminuto + " : " + vsegundo;
 
     dataHora.innerHTML = dataFormat + " - " + horaFormat;
@@ -151,3 +184,25 @@ function atualizaRelogio(){
 
     setTimeout("atualizaRelogio()",1000);
 }
+
+/* Botões limpar e imprimir horas */
+
+const resetButton = document.querySelector('#reset-button');
+const printButton = document.querySelector('#print-button');
+
+resetButton.addEventListener('click', () => {
+    
+    if (localStorage.hasOwnProperty("registroponto")) {
+        dados = JSON.parse(localStorage.getItem("registroponto"));
+        
+        if (dados.name == inputName.value) {
+            if (dados.password == inputPassword.value) {
+
+            } else {
+                alert("Senha para o úsuario incorreta!");
+            }
+        }
+    } else {
+        
+    }
+});
